fdf4c992bfb865388f96c41ec77148b8e8fe43e8
Rajesh Renganathan <rajesh.favour@gmail.com>
2022-02-24 19:19:21 +0000

    initial commit

---
+++ src/main/java/com/rajgates/demo/AzureSpringDemoApplication.java
@@ -0,0 +1,20 @@
+package com.rajgates.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureSpringDemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "This is my first spring azure test deployment";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringDemoApplication.class, args);
+	}
+
+}
